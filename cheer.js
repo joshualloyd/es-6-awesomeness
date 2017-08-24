7f6a20fbc8d473954af867c622b27040e9a70db3
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-08-24 09:50:04 -0500

    capitals and final cheer occurs

--- cheer.js
+++ cheer.js
@@ -8,13 +8,21 @@ const fullName = [...firstName, ...lastName];
 let counter = 0;
 
 const cheer = () => {
-
     if (counter < fullName.length) {
         setTimeout(function() {
-            console.log(`give me a ${fullName[counter]}`);
+            console.log(`give me a ${fullName[counter].toLocaleUpperCase()}!`);
             counter++;
             return cheer();
         }, 1000);
+    } if (counter === fullName.length ) {
+        setTimeout(function() {
+            console.log('What does that spell?');            
+        }, 1000);
+
+        setTimeout(function() {
+            let capitalName = `${firstName} ${lastName} !`.toUpperCase();
+            console.log(capitalName);
+        }, 2000);
     }
 };
 
